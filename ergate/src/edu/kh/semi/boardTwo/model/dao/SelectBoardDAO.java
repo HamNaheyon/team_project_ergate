4f5de09ccc2c4ae8cb67e3e4f7ce77208f2ad0ec
gkwld <gkwld@DESKTOP-65CUBRQ>
2021-07-02 17:03:50 +0900

    문의사항 최종, 상세조회 X

---
+++ ergate/src/edu/kh/semi/boardTwo/model/dao/SelectBoardDAO.java
@@ -0,0 +1,48 @@
+package edu.kh.semi.boardTwo.model.dao;
+
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import edu.kh.semi.boardTwo.model.vo.Board;
+
+public class SelectBoardDAO {
+	
+	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	private Properties prop = null;
+	
+	public SelectBoardDAO() {
+		
+		String filePath = SelectBoardDAO.class.getResource("/edu/kh/semi/sql/board/selectboard-query.xml").getPath();
+		
+		try {
+			prop = new Properties();
+			
+			prop.loadFromXML(new FileInputStream(filePath));
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}	}
+	
+	/** 게시글 상세 조회 DAO
+	 * @param conn
+	 * @param boardNo
+	 * @return board
+	 * @throws Exception
+	 */
+	public Board selectBoard(Connection conn, int boardNo) {
+		
+		Board board = null;
+		
+		String sql = prop.getProperty("selectBoard");
+		
+		return null;
+	}
+
+}
